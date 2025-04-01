93559df50cfd1772f1c11a438d0a6a38bd44a3ba
quantum-waves <1173023446@qq.com>
2025-04-02 00:31:07 +0800

    Update OrderTest.java

--- UserAndOrder/java/OrderTest.java
+++ UserAndOrder/java/OrderTest.java
@@ -2,4 +2,8 @@ public class OrderTest {
     public static void main(String[] args) {
         System.out.println("这是Order功能...");
     }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
 }
